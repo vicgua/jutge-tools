vicgua/jutge-tools

commit d6cb53bba13e95700cc9b511f40967f6c6dc1163

Fixed bugs
- On compile: debug passed flag "-O2" instead of "-O0"
- On test: test output was not decoded and appeared as b'...'

JutgeTools/compilef.py
@@ -20,7 +20,7 @@ def compilef(strict=True, debug=True, compiler=None, sources=''):
     compiler_tpl = Template(compiler)
     output = Path(cwd.name.split('_')[0]).with_suffix('.x')
     if debug:
-        flags = shlex.split('-g -O2')
+        flags = shlex.split('-g -O0')
     else:
         flags = shlex.split('-DNDEBUG -O2')
 

JutgeTools/test.py
@@ -34,7 +34,8 @@ def test(cases=None, compile=True, strict=True, debug=True, diff=True,
         corfile = inpfile.with_suffix('.cor')
         try:
             inp = inpfile.open('r')
-            out = subprocess.check_output([str(executable)], stdin=inp)
+            out = subprocess.check_output([str(executable)], stdin=inp,
+                                          universal_newlines=True)
         except subprocess.CalledProcessError as ex:
             if verbose:
                 print()

commit d113b02cfbc0da3cab32d81a598affa9c7e34e0c

Fixed some typos and bugs
- A missing comma, of course.
- Missing brackets to make a string list instead of a string.
- An error with handling of binary strings introduced ironically by
  d6cb53b. Reverted to how it was before.

JutgeTools/_aux/config_file.py
@@ -25,7 +25,7 @@ CONFIG_VARIABLES = {
 
     # shrc
     'shrc.p1++ alias': (bool, 'p1_alias', True),
-    'shrc.p2++ alias': (bool, 'p2_alias', True)
+    'shrc.p2++ alias': (bool, 'p2_alias', True),
     'shrc.dl alias': (str, None, 'dl'),
 
     # test

JutgeTools/compilef.py
@@ -27,7 +27,7 @@ def compilef(strict=True, debug=True, compiler=None, standard='c++11',
     if standard not in VALID_STANDARDS:
         raise ValueError(standard + ' is not a recognised standard. Valid'
                                     ' values are ' + str(VALID_STANDARDS))
-    flags = '-std=' + str(standard)
+    flags = ['-std=' + str(standard)]
     if debug:
         flags += shlex.split('-g -D_GLIBCXX_DEBUG -O0')
     else:

JutgeTools/test.py
@@ -36,8 +36,7 @@ def test(cases=None, compile=True, strict=True, debug=True, diff=True,
         corfile = inpfile.with_suffix('.cor')
         try:
             inp = inpfile.open('r')
-            out = subprocess.check_output([str(executable)], stdin=inp,
-                                          universal_newlines=True)
+            out = subprocess.check_output([str(executable)], stdin=inp)
         except subprocess.CalledProcessError as ex:
             if verbose:
                 print()