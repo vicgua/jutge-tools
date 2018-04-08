077907bd09708c76bfc6a6ed787536f4af29b168
Victor <victorg590@gmail.com>
2018-04-08 21:20:27 +0200

    Moved the templates of 'skel' into package data, instead of inlining them

--- JutgeTools/_aux/file_templates.py
+++ JutgeTools/_aux/file_templates.py
@@ -1,42 +1,18 @@
-from textwrap import dedent as _dedent
+import pkg_resources
+from string import Template
 
-_main_template = '''\
-    #include <iostream>
-
-    using namespace std;
-
-    int main() {
-        // Code
-    }
-    '''
-
-_header_template = '''\
-    #ifndef {macroname}
-    #define {macroname}
-
-    // Code
-
-    #endif // {macroname}
-    '''
-
-_source_template = '''\
-    #include "{header}"
-
-    // Code
-    '''
-
-_source_no_header_template = '''\
-    // Code
-    '''
+def _get_skel(file):
+    return Template(pkg_resources.resource_string(
+        __name__, 'data/skel/' + file).decode('utf-8'))
 
 def main_template():
-    return _dedent(_main_template)
+    return _get_skel('main.cc').substitute()
 
 def header_template(macroname):
-    return _dedent(_header_template.format(macroname=macroname))
+    return _get_skel('header.hh').substitute(macroname=macroname))
 
 def source_template(header):
-    return _dedent(_source_template.format(header=header))
+    return _get_skel('source.cc').substitute(header=header))
 
 def source_no_header_template():
-    return _dedent(_source_no_header_template)
+    return _get_skel('source_no_header.cc').substitute()
---
+++ JutgeTools/data/skel/header.hh
@@ -0,0 +1,6 @@
+#ifndef $macroname
+#define $macroname
+
+// Code
+
+#endif // $macroname
